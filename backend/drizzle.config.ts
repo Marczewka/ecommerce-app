809d877fe67728c7e199bfebbd2c5b70afe10b41
Marczewka <grzes.marczewski@gmail.com>
2026-05-27 18:02:12 +0200

    fix: make drizzle.config work

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,7 +1,4 @@
 import { defineConfig } from "drizzle-kit";
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 export default defineConfig({
     schema: "./src/db/schema.ts",
@@ -9,5 +6,8 @@ export default defineConfig({
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
+        ssl: {
+            rejectUnauthorized: false,
+        },
     },
 });
